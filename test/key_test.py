cf67e55f08f4464a8ebf563c7ff951ff51b74316
Nazar <nazarfatichov@mail.ru>
2020-03-20 10:58:23 +0300

    update key tests

--- test/key_test.py
+++ test/key_test.py
@@ -5,8 +5,8 @@ from entity.key import Key
 class KeyTest(unittest.TestCase):
 
     def setUp(self):
-        self.key = Key(bytes.fromhex("0123456789abcdeffedcba9876543210"))
-        self.q_key_result = bytes.fromhex("20 9e 66 7f 3b cc 90 8b")
+        self.key = Key(bytes.fromhex("0123456789abcdef fedcba9876543210"))
+        self.q_key_result = bytes.fromhex("6767313854966973 0857065648eabe43")
 
     def test_q_key(self):
         self.assertEqual(self.q_key_result, self.key.q_key())
