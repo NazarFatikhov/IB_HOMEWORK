adf311f64cdd1cf0970f6e86a89d3a221fcdd838
Nazar <nazarfatichov@mail.ru>
2020-04-10 15:23:50 +0500

    fix cbc mode

--- entity/cbc_mode.py
+++ entity/cbc_mode.py
@@ -4,11 +4,44 @@ from entity.byte_utils import ByteUtils
 class CBC:
 
     def encrypt(x: bytes, key: bytes):
-        if(len(x) < 16):
+        if (len(x) < 16):
             raise Exception("text should contain more or equal than 16 bytes")
-        elif(len(key) != 16):
+        elif (len(key) != 16):
             raise Exception("text should have 16 bytes")
 
+        if (CBC.is_message_total(x)):
+            if (CBC.is_block_empty(CBC.get_16_bytes_block(x, 1))):
+                if (CBC.is_block_padding(CBC.get_16_bytes_block(x, 2))):
+                    text = x + int(0).to_bytes(16, "little")
+                    return CBC.encrypt_if_block_total(text, key)
+                else:
+                    return CBC.encrypt_if_block_total(x, key)
+            else:
+                return CBC.encrypt_if_block_total(x, key)
+        else:
+            return CBC.encrypt_if_block_not_total(x, key)
+
+    def encrypt_if_block_total(x: bytes, key: bytes):
+        count = len(x) // 16
+        blocks = []
+
+        for i in range(1, count + 1):
+            blocks.append(x[16 * (i - 1): 16 * i])
+
+        cipher_blocks = []
+        init_block = key
+        for i in range(len(blocks)):
+            cipher_block = ByteUtils.xor(blocks[i], init_block)
+            cipher_blocks.append(cipher_block)
+            init_block = cipher_block
+
+        result = bytes()
+        for block in cipher_blocks:
+            result += block
+
+        return result
+
+    def encrypt_if_block_not_total(x: bytes, key: bytes):
         count = len(x) // 16
         blocks = []
 
@@ -31,7 +64,7 @@ class CBC:
         cipher_blocks = []
         init_block = key
         for i in range(len(blocks)):
-            cipher_block = Camelia.encrypt(ByteUtils.xor(blocks[i], init_block), key)
+            cipher_block = ByteUtils.xor(blocks[i], init_block)
             cipher_blocks.append(cipher_block)
             init_block = cipher_block
 
@@ -40,3 +73,84 @@ class CBC:
             result += block
 
         return result
+
+    def decrypt(x: bytes, key: bytes):
+        if (len(x) < 16):
+            raise Exception("text should contain more or equal than 16 bytes")
+        elif (len(key) != 16):
+            raise Exception("text should have 16 bytes")
+        elif (len(x) % 16 != 0):
+            raise Exception("text should have total each block")
+
+        count = len(x) // 16
+        blocks = []
+
+        for i in range(1, count + 1):
+            blocks.append(x[16 * (i - 1): 16 * i])
+
+        cipher_blocks = []
+        init_block = key
+        for i in range(len(blocks)):
+            cipher_block = ByteUtils.xor(blocks[i], init_block)
+            cipher_blocks.append(cipher_block)
+            init_block = blocks[i]
+
+        result = bytes()
+        for block in cipher_blocks:
+            result += block
+
+        if (CBC.is_block_empty(CBC.get_16_bytes_block(result, 1))):
+            return CBC.handle_decrypt_if_last_block_empty(result)
+        else:
+            return result
+
+    def handle_decrypt_if_last_block_empty(x: bytes):
+        if (CBC.is_block_empty(CBC.get_16_bytes_block(x, 2))):
+            if(CBC.is_block_padding(CBC.get_16_bytes_block(x, 3))):
+                return x[:len(x) - 16]
+            else:
+                return x
+        else:
+            if(CBC.is_block_padding(CBC.get_16_bytes_block(x, 2))):
+                return CBC.pars_padding_block(x[:len(x) - 16])
+            else:
+                return x
+
+    def handle_decrypt_if_3_block_padding(x: bytes):
+        if(CBC.is_block_padding(CBC.get_16_bytes_block(x, 3))):
+            return x[len(x) - 16]
+        else:
+            return x
+
+
+    def is_message_total(x: bytes):
+        return len(x) % 16 == 0
+
+    def is_block_padding(x: bytes):
+        count = x[-1]
+        k = 0
+        i = -2
+        while x[i] == 0 and abs(i) < len(x):
+            k += 1
+            i -= 1
+        if k == count:
+            return True
+        return False
+
+    def pars_padding_block(x: bytes):
+        if (len(x) % 16 != 0):
+            raise Exception("block should have total blocks")
+        count = int(x[-1])
+        return x[: len(x) - 1 - count]
+
+    def get_16_bytes_block(x: bytes, n: int):
+        if (len(x) < n * 16):
+            raise Exception("sequence should have not less than {0} bytes".format(n * 16))
+        if (n < 1):
+            raise Exception("count should be more than {0}".format(0))
+        return x[16 * (len(x)//16 - n):16 * (len(x)//16 - n) + 16]
+
+    def is_block_empty(x: bytes):
+        if(len(x) != 16):
+            raise Exception("block should have  bytes")
+        return x == bytes.fromhex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00")
--- test/cbc_mode_test.py
+++ test/cbc_mode_test.py
@@ -4,12 +4,117 @@ from entity.cbc_mode import CBC
 
 class CBCTest(unittest.TestCase):
 
+    def test_cbc(self):
+        key = bytes.fromhex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00")
+        #1
+        input = bytes.fromhex("07 92 3A 39 EB 0A 81 7D 1C 4D 87 BD B8 2D 1F 1C "
+                              "48 CD 64 19 80 96 72 D2 34 92 60 D8 9A 08")
+        encrypt = CBC.encrypt(input, key)
+        decrypt = CBC.decrypt(encrypt, key)
+        print("#1")
+        print("input -           {0}".format(input.hex()))
+        print("encrypt -         {0}".format(encrypt.hex()))
+        print("expected_result - {0}".format(decrypt.hex()))
+        print("actual_result -   {0}".format(input.hex()))
+        print()
+
+        # 2
+        input = bytes.fromhex("07 92 3A 39 EB 0A 81 7D 1C 4D 87 BD B8 2D 1F 1C "
+                              "48 CD 64 19 80 96 72 D2 34 92 60 D8 9A 08 00 01")
+        encrypt = CBC.encrypt(input, key)
+        decrypt = CBC.decrypt(encrypt, key)
+        print("#2")
+        print("input -           {0}".format(input.hex()))
+        print("encrypt -         {0}".format(encrypt.hex()))
+        print("expected_result - {0}".format(decrypt.hex()))
+        print("actual_result -   {0}".format(input.hex()))
+        print()
+
+        # 3
+        input = bytes.fromhex("07 92 3A 39 EB 0A 81 7D 1C 4D 87 BD B8 2D 1F 1C "
+                              "48 CD 64 19 80 96 72 D2 34 92 60 D8 9A 08 00 01 "
+                              "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"
+                              "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00")
+        encrypt = CBC.encrypt(input, key)
+        decrypt = CBC.decrypt(encrypt, key)
+        print("#3")
+        print("input -           {0}".format(input.hex()))
+        print("encrypt -         {0}".format(encrypt.hex()))
+        print("expected_result - {0}".format(decrypt.hex()))
+        print("actual_result -   {0}".format(input.hex()))
+        print()
+
+        # 4
+        input = bytes.fromhex("07 92 3A 39 EB 0A 81 7D 1C 4D 87 BD B8 2D 1F 1C "
+                              "48 CD 64 19 80 96 72 D2 34 92 60 D8 9A 08 00 02 "
+                              "00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00")
+        encrypt = CBC.encrypt(input, key)
+        decrypt = CBC.decrypt(encrypt, key)
+        print("#4")
+        print("input -           {0}".format(input.hex()))
+        print("encrypt -         {0}".format(encrypt.hex()))
+        print("expected_result - {0}".format(decrypt.hex()))
+        print("actual_result -   {0}".format(input.hex()))
+        print()
+
+        self.assertTrue(True)
+
+
+
+
     def setUp(self):
         self.key = bytes.fromhex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00")
         self.x = bytes.fromhex("07 92 3A 39 EB 0A 81 7D 1C 4D 87 BD B8 2D 1F 1C 48 CD 64 19 80 96 72 D2 34 92 60 D8 9A 08")
-        self.y = bytes.fromhex("07 92 3A 39 EB 0A 81 7D 1C 4D 87 BD B8 2D 1F 1C 48 CD 64 19 80 96 72 D2 34 92 60 D8 9A 08 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00")
+        self.y = bytes.fromhex("07 92 3A 39 EB 0A 81 7D 1C 4D 87 BD B8 2D 1F 1C 48 CD 64 19 80 96 72 D2 34 92 60 D8 9A 08 00 02 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00")
+        self.result = bytes.fromhex("07923a39eb0a817d1c4d87bdb82d1f1c4f5f5e206b9cf3af28dfe76522251f1d4f5f5e206b9cf3af28dfe76522251f1d")
 
     def test_encrypt(self):
-        result = CBC.encrypt(self.x, self.key)
-        print(result)
-        self.assertEqual(len(self.y), len(result))
+        result = CBC.encrypt(self.y, self.key)
+        print(result.hex())
+
+    def test_decrypt(self):
+        x = bytes.fromhex("07923a39eb0a817d1c4d87bdb82d1f1c4f5f5e206b9cf3af28dfe76522251f1d4f5f5e206b9cf3af28dfe76522251f1d4f5f5e206b9cf3af28dfe76522251f1d")
+        result = CBC.decrypt(x, self.key)
+        print(result.hex())
+
+    def test_is_message_total(self):
+        self.assertTrue(CBC.is_message_total(self.y) and not CBC.is_message_total(self.x))
+
+    def test_is_block_padding(self):
+        ls = []
+        expect = []
+        ls.append(bytes.fromhex("07 92 3A 39 EB 00 00 01 "))
+        expect.append(False)
+        ls.append(bytes.fromhex("07 92 3A 39 EB 00 00 02 "))
+        expect.append(True)
+        ls.append(bytes.fromhex("07 92 3A 39 EB F1 F1 00"))
+        expect.append(True)
+        ls.append(bytes.fromhex("07 92 3A 39 EB F1 F1 CC"))
+        expect.append(False)
+
+        real = []
+        for b in ls:
+            real.append(CBC.is_block_padding(b))
+
+        self.assertEqual(real, expect)
+
+    def test_pars_padding_block(self):
+        x = bytes.fromhex("07 92 3A 39 EB 0A 81 7D 1C 4D 87 BD B8 2D 1F 1C 48 CD 64 19 80 96 72 D2 34 92 60 D8 9A 08 00 02")
+        result = CBC.pars_padding_block(x)
+        expected = bytes.fromhex("07 92 3A 39 EB 0A 81 7D 1C 4D 87 BD B8 2D 1F 1C 48 CD 64 19 80 96 72 D2 34 92 60 D8 9A 08")
+        self.assertEqual(result, expected)
+
+    def test_get_16_bytes_block(self):
+        expect = []
+        expect.append(bytes.fromhex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"))
+        expect.append(bytes.fromhex("48 CD 64 19 80 96 72 D2 34 92 60 D8 9A 08 00 01"))
+
+        real = []
+
+        for i in range(1, 2 + 1):
+            real.append(CBC.get_16_bytes_block(self.y, i))
+
+        self.assertEqual(expect, real)
+
+    def test_is_block_empty(self):
+        self.assertTrue(CBC.is_block_empty(bytes.fromhex("00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00")) and not CBC.is_block_empty(bytes.fromhex("00 00 30 00 00 00 00 00 00 00 00 00 00 00 00 00")))
